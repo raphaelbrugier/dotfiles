c2297c398ed75c228a1252a06d811039b0e10bd9
Raphael Brugier <raphael.brugier@gmail.com>
2021-05-09 13:25:27 +0200

    add aws functions

--- _zsh_custom/aws.zsh
+++ _zsh_custom/aws.zsh
@@ -1,10 +1,16 @@
-function aws_assume_role() {
+function a-whoami() {
+  aws sts get-caller-identity | jq .
+}
+
+function a-unassume-role() {
+  unset AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
+  echo "AWS session cleared"
+}
+
+function a-assume-role() {
 	if [[ -z "$1" ]]
 	then
-		unset  AWS_SESSION_TOKEN
-		unset  AWS_ACCESS_KEY_ID
-		unset  AWS_SECRET_ACCESS_KEY
-		echo "AWS session cleared"
+		a_unassume_role
 		return
 	fi
 	local role_arn=${1}
@@ -17,4 +23,13 @@ function aws_assume_role() {
   export AWS_SECRET_ACCESS_KEY=$(echo "$temp_role" | jq -r .Credentials.SecretAccessKey)
   export AWS_SESSION_TOKEN=$(echo "$temp_role" | jq -r .Credentials.SessionToken)
   echo AWS session set to assume role "$role_arn"
+}
+
+
+function a-assume-role-json() {
+  a_unassume_role
+  export AWS_ACCESS_KEY_ID=`cat credentials.json | jq .Credentials.AccessKeyId | sed 's/"//g'`
+  export AWS_SECRET_ACCESS_KEY=`cat credentials.json | jq .Credentials.SecretAccessKey | sed 's/"//g'`
+  export AWS_SESSION_TOKEN=`cat credentials.json | jq .Credentials.SessionToken | sed 's/"//g'`
+  aws sts get-caller-identity | jq .
 }
