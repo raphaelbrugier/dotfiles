377201991501e53e21ac634781b501e5e88f29bd
Raphael Brugier <raphael.brugier@gmail.com>
2025-04-28 21:45:21 +0200

    Add more git aliases

--- home/dot_config/private_fish/conf.d/git.fish
+++ home/dot_config/private_fish/conf.d/git.fish
@@ -1,12 +1,20 @@
 if status is-interactive
   abbr -a gss 'git status -s'
-  abbr -a --set-cursor='%' -- gcm 'git commit -m "%"'
+  abbr -a --set-cursor='%' -- gcmsg 'git commit -m "%"'
   abbr -a -- gp 'git push'
   abbr -a -- gpf 'git push -f'
   abbr -a -- gd 'git diff'
+  abbr -a -- gl 'git pull'
+  abbr -a -- gappend 'git commit --all --amend --no-edit'
+
+  # rebase
   abbr -a -- grb 'git rebase'
   abbr -a -- grbc 'git rebase --continue'
   abbr -a -- grba 'git rebase --abort'
   abbr -a -- grbi 'git rebase --interactive'
   abbr -a -- grbm 'git rebase origin/main'
+
+  # logs
+  abbr -a --  glog 'git log --oneline --decorate --graph'
+  abbr -a --  gloga  'git log --oneline --decorate --graph --all'
 end
